e262b4e427e48d8bad32b19f4180acd491fd100d
unknown <danasheer21111@gmail.com>
2023-02-12 20:15:09 +0300

    task2 done

--- challenge1.dart
+++ challenge1.dart
@@ -1,9 +1,13 @@
-/**
- * Task 1:
- * Create a function called `printName`
- * - that prints your name on the screen.
- */
+void main() {
+ printName();
+ printAge(birth:1995);
+ printHello(name:"dana",language: "fr");
+ printMax(num1: 4, num2: 8);
+}
+void printName(){
+  print("my name is dana ");
 
+}
 /**
  * Task 2:
  * Create a function called `printAge`
@@ -11,7 +15,11 @@
  * - and prints the age on the screen.
  * - Age = current year - birth
  */
+void printAge({double birth = 1}) {
+  double age = 2023 - birth;
+  print(" my age = $age");
 
+}
 /**
  * Task 3:
  * Create a function called `printHello`
@@ -22,10 +30,32 @@
  * -- fr: it should print `Bonjour NAME`
  * -- tr: it should print `Merhaba NAME`
  */
-
+void printHello({String name="" ,String language =""}){
+  if (language == "en" ){
+   print("Hello $name");
+  }
+  else if (language == "es"){
+    print("Hola $name");
+  }
+   else if (language == "fr"){
+    print(" Bonjour  $name");
+  }
+   else if (language == "tr"){
+    print("Merhaba $name");
+  }
+}
 /**
  * Task 4:
  * Create a function called `printMax`
  * - that takes two numbers as parameters,
  * - and prints out the bigger number
  */
+
+void printMax({int num1 = 0 , num2 =0 }){
+  if (num1 > num2){
+    print("$num1 is greeter than $num2");
+  }
+  else{
+    print("$num2 is greeter than $num1");
+  }
+}
--- challenge2.dart
+++ challenge2.dart
@@ -1,51 +1,57 @@
-/**
- * greet(name):
- * - receives a name,
- * - prints "Hello <name>"
- *
- * e.g.
- * greet("Hamza") => prints "Hello Hamza"
- */
-void greet(String name) {
-  // Your code here
+void main(){
+  greet(name: "Hamza");
+  print(isOdd(5));
+  print(squareOrDouble(9));
 }
 
-/**
- * isOdd(n):
- * - receives a number n,
- * - returns true if n is odd, false otherwise.
- *
- * e.g.
- * isOdd(7) -> true
- * isOdd(10) -> false
- */
+void greet({String name=""}) {
+  print("Hello $name");
+}
+
+
 bool isOdd(int n) {
-  // Your code here
+  if (n%2 == 0){
+   return false;
+  }
+  else{
+    return true;
+  }
 }
 
-/**
- * oddsSmallerThan(n):
- * - receives a number n,
- * - returns the number of ODD numbers smaller than n.
- *
- * e.g.
- * oddsSmallerThan(7) -> 3
- * oddsSmallerThan(15) -> 7
- */
-int oddsSmallerThan(int n) {
-  // Your code here
-}
 
-/**
- * squareOrDouble(n):
- * - receives a number n,
- * - returns n's square if n is odd,
- * - returns n's double if n is even.
- *
- * e.g.
- * squareOrDouble(16) -> 32
- * squareOrDouble(9) -> 81
- */
-int squareOrDouble(int n) {
+// void oddsSmallerThan(n){
+
+// }
+    
+//  * - receives a number n,
+//  * - returns the number of ODD numbers smaller than n.
+//  *
+//  * e.g.
+//  * oddsSmallerThan(7) -> 3
+//  * oddsSmallerThan(15) -> 7
+//  */
+// // int oddsSmallerThan(int n) {
   // Your code here
+// }
+
+
+int squareOrDouble(n){
+  if (n%2 == 0){
+    return n*2;
+  }
+  else{
+    return n*n;
+  }
+
 }
+//  * - receives a number n,
+//  * - returns n's square if n is odd,
+//  * - returns n's double if n is even.
+//  *
+//  * e.g.
+//  * squareOrDouble(16) -> 32
+//  * squareOrDouble(9) -> 81
+//  */
+// int squareOrDouble(int n) {
+//   // Your code here
+// }
